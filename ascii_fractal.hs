4f0276b76ab981d7fdd46cdfac17ae36cbf6de5b
Than2000 <nathaniel.gerlek@gmail.com>
2019-06-28 17:51:11 -0600

    Increased sierpinski resolution

--- ascii_fractal.hs
+++ ascii_fractal.hs
@@ -447,11 +447,10 @@ vtcSierpinski gd v@(V2 x y)
   | x<0 || x>1 || y<0 || y>1 = minShadeChar
   | any (withinRange gd v) $ truncatedList gd = maxShadeChar
   | otherwise = minShadeChar
-truncatedList gd = take len $ sierpinskiList'
-    where iters' = 10 * (getIters gd)
-          len = length randList
+truncatedList gd = take iters' $ drop iters' $ sierpinskiList'    --Hardcoded parameter
+          where iters' = 10 * (getIters gd)
 withinRange gd (V2 x1 y1) (V2 x2 y2) = abs (x2-x1) <= width && abs (y2-y1) <= height
-    where (width, height) = (pxlW gd, pxlH gd)
+    where (width, height) = (0.75*(pxlW gd), (0.75*pxlH gd))    --Hardcoded parameter
 
 sierpinskiList :: (Vector2, Int) -> [Vector2]
 sierpinskiList (v, n) = v : (sierpinskiList $ nextSierpinski (v, n))
@@ -469,9 +468,19 @@ trans1 = Transformation (0.5, 0.0, 0.0, 0.5) (V2 0.5 0)
 trans2 = Transformation (0.5, 0.0, 0.0, 0.5) (V2 0.25 ((sqrt 3)/4))
 
 
---WIP - Should generate a pseudorandom number, 0..2
+--Generates a pseudorandom digit
 rand :: Int -> Int
-rand n = randList !! n
+rand n = randList !! (n `mod` (length randList))
 
 
-randList = [3,1,4,1,5,9,2,6,5,3,5,8,9,7,9,3,2,3,8,4,6,2,6,4,3,3,8,3,2,7,9,5,0,2,8,8,4,1,9,7]
+randList = [3,1,4,1,5,9,2,6,5,3,5,8,9,7,9,3,2,3,8,4,6,2,6,4,3,3,8,3,2,7,9,5,0,2,8,8,4,1,9,7,
+            1,6,9,3,9,9,3,7,5,1,0,5,8,2,0,9,7,4,9,4,4,5,9,2,3,0,7,8,1,6,4,0,6,2,8,6,2,0,8,9,9,8,6,2,8,0,
+            3,4,8,2,5,3,4,2,1,1,7,0,6,7,9,8,2,1,4,8,0,8,6,5,1,3,2,8,2,3,0,6,6,4,7,0,9,3,8,4,4,6,0,9,5,5,
+            0,5,8,2,2,3,1,7,2,5,3,5,9,4,0,8,1,2,8,4,8,1,1,1,7,4,5,0,2,8,4,1,0,2,7,0,1,9,3,8,5,2,1,1,0,5,
+            5,5,9,6,4,4,6,2,2,9,4,8,9,5,4,9,3,0,3,8,1,9,6,4,4,2,8,8,1,0,9,7,5,6,6,5,9,3,3,4,4,6,1,2,8,4,
+            7,5,6,4,8,2,3,3,7,8,6,7,8,3,1,6,5,2,7,1,2,0,1,9,0,9,1,4,5,6,4,8,5,6,6,9,2,3,4,6,0,3,4,8,6,1,
+            0,4,5,4,3,2,6,6,4,8,2,1,3,3,9,3,6,0,7,2,6,0,2,4,9,1,4,1,2,7,3,7,2,4,5,8,7,0,0,6,6,0,6,3,1,5,
+            5,8,8,1,7,4,8,8,1,5,2,0,9,2,0,9,6,2,8,2,9,2,5,4,0,9,1,7,1,5,3,6,4,3,6,7,8,9,2,5,9,0,3,6,0,0,
+            1,1,3,3,0,5,3,0,5,4,8,8,2,0,4,6,6,5,2,1,3,8,4,1,4,6,9,5,1,9,4,1,5,1,1,6,0,9,4,3,3,0,5,7,2,7,
+            0,3,6,5,7,5,9,5,9,1,9,5,3,0,9,2,1,8,6,1,1,7,3,8,1,9,3,2,6,1,1,7,9,3,1,0,5,1,1,8,5,4,8,0,7,4,
+            4,6,2,3,7,9,9,6,2,7,4,9]
