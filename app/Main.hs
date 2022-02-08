43244250ba021888381b9840dbecc5a2c645856e
Than2000 <nathaniel.gerlek@gmail.com>
2022-02-07 23:14:07 -0800

    Fixed module names and rebuilt

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Lib
+import AsciiFractal
 
 main :: IO ()
-main = someFunc
+main = afMain
--- src/AsciiFractal.hs
+++ src/AsciiFractal.hs
@@ -1,10 +1,11 @@
+module AsciiFractal (afMain) where
 import Data.Complex
 import Data.Either
 import Data.Maybe
 --import System.Random
 import Text.Read
 
-main = do
+afMain = do
   putStrLn "Compiled."
   center defaultGD ["0", "0"]
 
